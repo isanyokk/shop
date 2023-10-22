290cea3431e334322c4e222dbda5204059a1f959
iSanyok <isanyokk@yandex.ru>
2023-10-23 00:24:08 +0300

    hueta s failami

--- src/Entity/Image.php
+++ src/Entity/Image.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\ImageRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 #[ORM\Entity(repositoryClass: ImageRepository::class)]
 class Image
@@ -14,45 +15,24 @@ class Image
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
-    private ?string $filepath = null;
+    private ?string $path = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $webpath = null;
-
-    #[ORM\ManyToOne(inversedBy: 'images')]
+    #[ORM\ManyToOne(cascade: ['persist'], inversedBy: 'images')]
     private ?Product $product = null;
-
-    #[ORM\Column]
-    private ?int $filesize = null;
-
-    #[ORM\Column(length: 255)]
-    private ?string $extension = null;
 
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getFilepath(): ?string
+    public function getPath(): ?string
     {
-        return $this->filepath;
+        return $this->path;
     }
 
-    public function setFilepath(string $filepath): static
+    public function setPath(string $path): static
     {
-        $this->filepath = $filepath;
-
-        return $this;
-    }
-
-    public function getWebpath(): ?string
-    {
-        return $this->webpath;
-    }
-
-    public function setWebpath(string $webpath): static
-    {
-        $this->webpath = $webpath;
+        $this->path = $path;
 
         return $this;
     }
@@ -65,30 +45,6 @@ class Image
     public function setProduct(?Product $product): static
     {
         $this->product = $product;
-
-        return $this;
-    }
-
-    public function getFilesize(): ?int
-    {
-        return $this->filesize;
-    }
-
-    public function setFilesize(int $filesize): static
-    {
-        $this->filesize = $filesize;
-
-        return $this;
-    }
-
-    public function getExtension(): ?string
-    {
-        return $this->extension;
-    }
-
-    public function setExtension(string $extension): static
-    {
-        $this->extension = $extension;
 
         return $this;
     }
