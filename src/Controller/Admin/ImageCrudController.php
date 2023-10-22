c05410f5b8b4b743eff3d8a0555cbe9d7c43a790
iSanyok <isanyokk@yandex.ru>
2023-10-22 18:42:07 +0300

    commit2

--- src/Controller/Admin/ImageCrudController.php
+++ src/Controller/Admin/ImageCrudController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Image;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 
 class ImageCrudController extends AbstractCrudController
 {
@@ -12,14 +13,14 @@ class ImageCrudController extends AbstractCrudController
         return Image::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            ImageField::new('filepath')
+                ->setLabel('Изображение')
+                ->setUploadDir('public/uploads/images/product')
+                ->setBasePath('uploads/images/product')
+                ->setUploadedFileNamePattern('[year]-[month]-[day]_[ulid].[extension]'),
         ];
     }
-    */
 }
--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -6,13 +6,13 @@ use App\Entity\Product;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-use Symfony\Component\Form\Extension\Core\Type\CurrencyType;
 
 class ProductCrudController extends AbstractCrudController
 {
